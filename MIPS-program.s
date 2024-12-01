66e5b1a82a3f7fb082034f1f8d6f74a78687c99a
Thane Douglass <144848584+thanedouglass@users.noreply.github.com>
2024-12-01 22:50:58 +0000

    print prompt for user input and setting up input validation loop

--- MIPS-program.s
+++ MIPS-program.s
@@ -2,3 +2,11 @@
     prompt: .asciiz "Please enter a positive integer >= 25: "
     error:  .asciiz "Illegal Number!\n"
     space:  .asciiz " "
+
+.text
+    .globl main
+main:
+    input_loop:
+        li $v0, 4
+        la $a0, prompt
+        syscall
