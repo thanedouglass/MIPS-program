96ed0ee750200c559dd5431f494fc3f946fb8be5
Thane Douglass <144848584+thanedouglass@users.noreply.github.com>
2024-12-01 22:55:32 +0000

    finish setting up input validation loop and final error handling code

--- MIPS-program.s
+++ MIPS-program.s
@@ -10,3 +10,10 @@ main:
         li $v0, 4
         la $a0, prompt
         syscall
+
+        li $v0, 5
+        syscall
+
+        move $t0, $v0       # Store input in $t0
+        blt $t0, 25, error_handler   # Branch if input < 25
+        blez $t0, error_handler      # Branch if input <= 0
